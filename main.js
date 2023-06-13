14b533c4f84b23aab808b4909e5ddc12f3cfd510
nzm.allahverdyv03@gmail.com <nzm.allahverdyv03@gmail.com>
2023-06-13 21:40:48 +0400

    Calculator added

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
-const operator = prompt('Enter operator (either +, -, * or /): ');
+const operator = prompt("Enter operator: ");
 
-const number1 = parseFloat(prompt('Enter first number: '));
-const number2 = parseFloat(prompt('Enter second number: '));
+const number1 = parseFloat(prompt("Enter first number: "));
+const number2 = parseFloat(prompt("Enter second number: "));
 
 let result;
 
-if (operator == '+') {
-    result = number1 + number2;
-}
-else if (operator == '-') {
-    result = number1 - number2;
-}
-else if (operator == '*') {
-    result = number1 * number2;
-}
-else {
-    result = number1 / number2;
+if (operator == "+") {
+  result = number1 + number2;
+} else if (operator == "-") {
+  result = number1 - number2;
+} else if (operator == "*") {
+  result = number1 * number2;
+} else {
+  result = number1 / number2;
 }
 
 alert(`${number1} ${operator} ${number2} = ${result}`);
